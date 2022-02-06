5b9cc9ce7d835781cc9ebcc967189dcfd24a1880
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-02-06 12:45:30 -0600

    Flake8 nmma/tests

--- nmma/__init__.py
+++ nmma/__init__.py
@@ -9,16 +9,14 @@ NMMA: A nuclear multi-messenger Bayesian inference library.
 from __future__ import absolute_import
 import os
 
-from . import em
-from . import joint
-from . import eos
 
 def get_version_information():
-    version_file = os.path.join(os.path.dirname(__file__), '.version')
+    version_file = os.path.join(os.path.dirname(__file__), ".version")
     try:
-        with open(version_file, 'r') as f:
+        with open(version_file, "r") as f:
             return f.readline().rstrip()
     except EnvironmentError:
         print("No version information file '.version' found")
+
 
 __version__ = get_version_information()
