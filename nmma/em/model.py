4f8e966c52f985459e04f84a587fb9fc0b68d2f8
Peter T. H. Pang <thopang@gmail.com>
2022-03-23 21:58:34 +0100

    Fix PL_BB_fixedT 4
    Former-commit-id: 7073191b078b5bcd338b255cb80d36f8fe982766

--- nmma/em/model.py
+++ nmma/em/model.py
@@ -36,7 +36,7 @@ model_parameters_dict = {
     "Piro2021": ["log10_Menv", "log10_Renv", "log10_Ee"],
     "Me2017": ["log10_Mej", "log10_vej", "beta", "log10_kappa_r"],
     "Bu2022mv": ["log10_mej_dyn", "vej_dyn", "log10_mej_wind", "vej_wind", "KNtheta"],
-    "PL_BB_fixedT": ["bb_luminosity", "temperature", "beta", "powerlaw_mag", "powerlaw_filt_ref"]
+    "PL_BB_fixedT": ["bb_luminosity", "temperature", "beta", "powerlaw_mag"]
 }
 
 
@@ -668,6 +668,6 @@ class SimpleKilonovaLightCurveModel(object):
         if self.model == 'Me2017':
             _, lbol, mag = utils.metzger_lc(sample_times, param_dict)
         elif self.model == 'PL_BB_fixedT':
-            _, lbol, mag = utils.powerlaw_blackbody_constant_temperature_lc(sample_timesm,
+            _, lbol, mag = utils.powerlaw_blackbody_constant_temperature_lc(sample_times,
                                                                             param_dict)
         return lbol, mag
