283c8af1d2978119c25a9526325879dc20e5dc8b
Michael Coughlin <michael.w.coughlin@gmail.com>
2022-04-26 16:22:47 -0500

    Add Bu2019nsbh as a training option

--- nmma/em/model_parameters.py
+++ nmma/em/model_parameters.py
@@ -95,3 +95,30 @@ def Bu2022mv(data):
         data_out[key] = {**data_out[key], **data[key]}
 
     return data_out, parameters
+
+
+def Bu2019nsbh(data):
+
+    data_out = {}
+
+    parameters = ["log10_mej_dyn", "log10_mej_wind", "KNtheta"]
+    parameters_idx = [1, 2, 4]
+    magkeys = data.keys()
+    for jj, key in enumerate(magkeys):
+        rr = [
+            float(x)
+            for x in re.findall(
+                r"[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", key
+            )
+        ]
+
+        # Best to interpolate mass in log10 space
+        rr[1] = np.log10(rr[1])
+        rr[2] = np.log10(rr[2])
+
+        data_out[key] = {
+            param: rr[idx] for param, idx in zip(parameters, parameters_idx)
+        }
+        data_out[key] = {**data_out[key], **data[key]}
+
+    return data_out, parameters
