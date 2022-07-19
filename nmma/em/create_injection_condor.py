7f0bca14ae9a1c75972778997cd5a2773085b0bf
Michael Coughlin <michael.coughlin@ligo.org>
2022-07-19 12:56:08 -0700

    Use f-strings

--- nmma/em/create_injection_condor.py
+++ nmma/em/create_injection_condor.py
@@ -169,23 +169,7 @@ def main():
                 job_number = job_number + 1
 
                 fid1.write(
-                    "%s --model %s --svd-path /home/%s/gwemlightcurves/output/svdmodels --outdir %s --label injection_%s --prior analysis.prior --tmin 0 --tmax 20 --dt 0.5 --error-budget 1 --nlive %d --Ebv-max 0 --injection %s --injection-num %s --injection-detection-limit %s --injection-outfile %s --generation-seed 42 --filters %s --plot --remove-nondetections --photometry-augmentation --photometry-augmentation-N-points %d --photometry-augmentation-filters %s --photometry-augmentation-seed %d\n"
-                    % (
-                        lc_analysis,
-                        args.model,
-                        os.environ["USER"],
-                        outdir,
-                        args.model,
-                        args.nlive,
-                        args.injection_file,
-                        str(index),
-                        args.injection_detection_limit,
-                        injfile,
-                        args.filters,
-                        args.photometry_augmentation_N_points,
-                        args.photometry_augmentation_filters,
-                        ii,
-                    )
+                    f"{lc_analysis} --model {args.model} --svd-path ./svdmodels --outdir {outdir} --label injection_{args.model} --prior {args.prior_file} --tmin 0 --tmax 20 --dt 0.5 --error-budget 1 --nlive {args.nlive} --Ebv-max 0 --injection {args.injection_file} --injection-num {str(index)} --injection-detection-limit {args.injection_detection_limit} --injection-outfile {injfile} --generation-seed 42 --filters {args.filters} --plot --remove-nondetections --photometry-augmentation --photometry-augmentation-N-points {args.photometry_augmentation_N_points} --photometry-augmentation-filters {args.photometry_augmentation_filters} --photometry-augmentation-seed {ii}\n"
                 )
         else:
             priors = bilby.gw.prior.PriorDict(args.prior_file)
@@ -203,20 +187,7 @@ def main():
             job_number = job_number + 1
 
             fid1.write(
-                "%s --model %s --svd-path /home/%s/gwemlightcurves/output/svdmodels --outdir %s --label injection_%s --prior analysis.prior --tmin 0 --tmax 20 --dt 0.5 --error-budget 1 --nlive %d --Ebv-max 0 --injection %s --injection-num %s --injection-detection-limit %s --injection-outfile %s --generation-seed 42 --filters %s --plot --remove-nondetections\n"
-                % (
-                    lc_analysis,
-                    args.model,
-                    os.environ["USER"],
-                    outdir,
-                    args.model,
-                    args.nlive,
-                    args.injection_file,
-                    str(index),
-                    args.injection_detection_limit,
-                    injfile,
-                    args.filters,
-                )
+                f"{lc_analysis} --model {args.model} --svd-path ./svdmodels --outdir %s --label injection_{args.model} --prior {args.prior_file} --tmin 0 --tmax 20 --dt 0.5 --error-budget 1 --nlive {args.nlive} --Ebv-max 0 --injection {args.injection_file} --injection-num {str(index)} --injection-detection-limit {args.injection_detection_limit} --injection-outfile {injfile} --generation-seed 42 --filters {args.filters} --plot --remove-nondetections\n"
             )
 
     fid.close()
@@ -228,31 +199,11 @@ def main():
 
     if args.photometry_augmentation:
         fid.write(
-            "arguments = --model %s --svd-path /home/%s/gwemlightcurves/output/svdmodels --outdir $(OUTDIR) --label injection_%s --prior analysis.prior --tmin 0 --tmax 20 --dt 0.5 --error-budget 1 --nlive %d --Ebv-max 0 --injection %s --injection-num $(INJNUM) --injection-detection-limit %s --injection-outfile $(INJOUT) --generation-seed 42 --filters %s --plot --remove-nondetections --photometry-augmentation --photometry-augmentation-N-points %d --photometry-augmentation-filters %s --photometry-augmentation-seed $(SEED)\n"
-            % (
-                args.model,
-                os.environ["USER"],
-                args.model,
-                args.nlive,
-                args.injection_file,
-                args.injection_detection_limit,
-                args.filters,
-                args.photometry_augmentation_N_points,
-                args.photometry_augmentation_filters,
-            )
+            f"arguments = --model {args.model} --svd-path ./svdmodels --outdir $(OUTDIR) --label injection_{args.model} --prior {args.prior_file} --tmin 0 --tmax 20 --dt 0.5 --error-budget 1 --nlive {args.nlive} --Ebv-max 0 --injection {args.injection_file} --injection-num $(INJNUM) --injection-detection-limit {args.injection_detection_limit} --injection-outfile $(INJOUT) --generation-seed 42 --filters {args.filters} --plot --remove-nondetections --photometry-augmentation --photometry-augmentation-N-points {args.photometry_augmentation_N_points} --photometry-augmentation-filters {args.photometry_augmentation_filters} --photometry-augmentation-seed $(SEED)\n"
         )
     else:
         fid.write(
-            "arguments = --model %s --svd-path /home/%s/gwemlightcurves/output/svdmodels --outdir $(OUTDIR) --label injection_%s --prior analysis.prior --tmin 0 --tmax 20 --dt 0.5 --error-budget 1 --nlive %d --Ebv-max 0 --injection %s --injection-num $(INJNUM) --injection-detection-limit %s --injection-outfile $(INJOUT) --generation-seed 42 --filters %s --plot --remove-nondetections\n"
-            % (
-                args.model,
-                os.environ["USER"],
-                args.model,
-                args.nlive,
-                args.injection_file,
-                args.injection_detection_limit,
-                args.filters,
-            )
+            f"arguments = --model {args.model} --svd-path ./svdmodels --outdir $(OUTDIR) --label injection_{args.model} --prior {args.prior_file} --tmin 0 --tmax 20 --dt 0.5 --error-budget 1 --nlive {args.nlive} --Ebv-max 0 --injection {args.injection_file} --injection-num $(INJNUM) --injection-detection-limit {args.injection_detection_limit} --injection-outfile $(INJOUT) --generation-seed 42 --filters {args.filters} --plot --remove-nondetections\n"
         )
     fid.write('requirements = OpSys == "LINUX"\n')
     fid.write("request_memory = 8192\n")
