3147cebfa3e5982c368fe565e1955daf15f89d40
Michael Coughlin <michael.w.coughlin@gmail.com>
2023-07-24 11:07:00 -0500

    Version 0.0.16

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ def readfile(filename):
     return filecontents
 
 
-VERSION = "0.0.15"
+VERSION = "0.0.16"
 version_file = write_version_file(VERSION)
 long_description = get_long_description()
 
