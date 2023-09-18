503af4e5ec4feb228a4f4229fd2e6755d9362fdf
sahiljhawar <55475299+sahiljhawar@users.noreply.github.com>
2023-09-19 01:26:08 +0200

    Update training.py

--- nmma/tests/training.py
+++ nmma/tests/training.py
@@ -25,7 +25,7 @@ def test_training():
     workingDir = os.path.dirname(__file__)
 
     dataDir = os.path.join(workingDir, 'data/bulla')
-    Modelpath=os.path.join(workingDir, '../../svdmodels/')
+    ModelPath = "svdtrainingmodel"
     filenames = glob.glob(f"{dataDir}/*.dat")
 
     data = io.read_photometry_files(filenames, filters=filts)
