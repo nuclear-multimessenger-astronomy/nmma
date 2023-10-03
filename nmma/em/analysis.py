baff96f50968434f0209675c6b43819a5153b014
sahiljhawar <55475299+sahiljhawar@users.noreply.github.com>
2023-10-03 17:23:35 +0200

    ln -> log
    Co-authored-by: Tyler Barna <57914086+tylerbarna@users.noreply.github.com>

--- nmma/em/analysis.py
+++ nmma/em/analysis.py
@@ -744,7 +744,7 @@ def analysis(args):
 
     if args.bestfit:
         bestfit_to_write = bestfit_params.copy()
-        bestfit_to_write["ln_bayes_factor"] = result.log_bayes_factor
+        bestfit_to_write["log_bayes_factor"] = result.log_bayes_factor
         bestfit_to_write["ln_bayes_factor_err"] = result.log_evidence_err
         bestfit_to_write["Best fit index"] = int(bestfit_idx)
         bestfit_to_write["Magnitudes"] = {i: mag[i].tolist() for i in mag.keys()}
