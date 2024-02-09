ef5236d2a482728cd4b0c298892efbdfbcc135e8
Brian Healy <brian.francis.healy@gmail.com>
2024-02-09 14:02:28 -0600

    Split download/refresh models tests by gitlab/zenodo

--- nmma/tests/models.py
+++ nmma/tests/models.py
@@ -1,14 +1,7 @@
 from ..utils.models import get_model, refresh_models_list
 
 
-def test_download_model():
-    # Test that we can download a model from Zenodo
-    files, filters = get_model(
-        model_name="Ka2017_tf", filters=["ztfg"], source="zenodo"
-    )
-    assert len(files) == 2
-    assert len(filters) == 1
-
+def test_download_model_gitlab():
     # Test that we can download a model from GitLab
     files, filters = get_model(
         model_name="LANLTP1_tf", filters=["ztfg"], source="gitlab"
@@ -17,11 +10,22 @@ def test_download_model():
     assert len(filters) == 1
 
 
-def test_refresh_models_list():
-    # Test that we can refresh the models list from Zenodo
-    models = refresh_models_list(source="zenodo")
-    assert len(models) > 0
+def test_download_model_zenodo():
+    # Test that we can download a model from Zenodo
+    files, filters = get_model(
+        model_name="Ka2017_tf", filters=["ztfg"], source="zenodo"
+    )
+    assert len(files) == 2
+    assert len(filters) == 1
 
+
+def test_refresh_models_list_gitlab():
     # Test that we can refresh the models list from GitLab
     models = refresh_models_list(source="gitlab")
+    assert len(models) > 0
+
+
+def test_refresh_models_list_zenodo():
+    # Test that we can refresh the models list from Zenodo
+    models = refresh_models_list(source="zenodo")
     assert len(models) > 0
