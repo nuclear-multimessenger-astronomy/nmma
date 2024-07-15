2f8dcefc6006f57d4e1a10f34fbb0562ba7c3622
malinadesai <108042357+malinadesai@users.noreply.github.com>
2024-07-15 18:53:57 -0400

    Update inference.py
    added functionality if truth is not available.

--- nmma/mlmodel/inference.py
+++ nmma/mlmodel/inference.py
@@ -23,11 +23,7 @@ from matplotlib.lines import Line2D
 num_points = 121
 
 def cast_as_bilby_result(samples, truth, priors):
-    injections = dict.fromkeys({'log10_mej', 'log10_vej', 'log10_Xlan'})
-    injections['log10_mej'] = float(truth.numpy()[0])
-    injections['log10_vej'] = float(truth.numpy()[1])
-    injections['log10_Xlan'] = float(truth.numpy()[2])
-
+    
     posterior = dict.fromkeys({'log10_mej', 'log10_vej', 'log10_Xlan'})
     samples_numpy = samples.numpy()
     posterior['log10_mej'] = samples_numpy.T[0].flatten()
@@ -35,11 +31,25 @@ def cast_as_bilby_result(samples, truth, priors):
     posterior['log10_Xlan'] = samples_numpy.T[2].flatten()
     posterior = pd.DataFrame(posterior)
     
-    return bilby.result.Result(
-        label="test_data",
-        injection_parameters=injections,
-        posterior=posterior,
-        search_parameter_keys=list(injections.keys()),
-        priors=priors
-    )
+    if truth = None:
+        result = bilby.result.Result(
+            label="test_data",
+            posterior=posterior,
+            search_parameter_keys=list(injections.keys()),
+            priors=priors
+        )
+    else: 
+        injections = dict.fromkeys({'log10_mej', 'log10_vej', 'log10_Xlan'})
+        injections['log10_mej'] = float(truth.numpy()[0])
+        injections['log10_vej'] = float(truth.numpy()[1])
+        injections['log10_Xlan'] = float(truth.numpy()[2])
 
+        result = bilby.result.Result(
+            label="test_data",
+            injection_parameters=injections,
+            posterior=posterior,
+            search_parameter_keys=list(injections.keys()),
+            priors=priors
+        )
+    
+    return result
