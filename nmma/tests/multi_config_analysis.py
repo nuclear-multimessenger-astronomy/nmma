ad623327b9dc6bf2efdfa98d32f19a1fb20cab87
Sahil Jhawar <sahil.jhawar448@gmail.com>
2024-10-17 22:08:22 +0200

    remove file deletion

--- nmma/tests/multi_config_analysis.py
+++ nmma/tests/multi_config_analysis.py
@@ -9,16 +9,6 @@ from ..em import multi_config_analysis
 WORKING_DIR = os.path.dirname(__file__)
 
 
-@pytest.fixture(autouse=True)
-def cleanup_outdir(args):
-    yield
-    files = [os.path.join(WORKING_DIR, "outdir_1"), os.path.join(WORKING_DIR, "outdir_2")]
-    for file in files:
-        
-        assert os.path.exists(os.path.join(file, "injection_posterior_samples.dat"))
-        if os.path.exists(file):
-            shutil.rmtree(file)
-
 
 @pytest.fixture(scope="module")
 def args():
